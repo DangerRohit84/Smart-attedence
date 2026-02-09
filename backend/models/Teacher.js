1fa7bf4e3dbf7bc7312e0b2070cb81a545793d0a
D Rohit <desinarohit2007@gmail.com>
2026-02-09 22:43:33 +0530

    Update Teacher.js

--- backend/models/Teacher.js
+++ backend/models/Teacher.js
@@ -2,10 +2,11 @@
 import mongoose from 'mongoose';
 
 const TeacherSchema = new mongoose.Schema({
-  employeeId: { type: String, required: true, unique: true },
+  identifier: { type: String, required: true, unique: true }, // e.g., EMP101
   name: { type: String, required: true },
   password: { type: String, required: true },
   department: String,
+  role: { type: String, default: 'TEACHER' }
 }, { timestamps: true });
 
 export default mongoose.models.Teacher || mongoose.model('Teacher', TeacherSchema);
